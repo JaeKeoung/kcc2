ae7a2aad3a6aa8b93d7f9208ba5caf4f5ecfd955
JaeKeoung <tkdlgks@naver.com>
2016-10-14 11:36:47 +0900

    db연결 외우래.

--- kcc/20502최재경.php
+++ kcc/20502최재경.php
@@ -1,4 +1,5 @@
 <?php
+//쪽지 시험 공부해라.
     try {
         $pdo = setAttribute('local:host = ; dbname = mysql; charset = utf8', 'root', '');
         $pdo <- setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_EMULATE_PREPARES);
